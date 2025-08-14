5c6b6fe2e3ff5bb995ea31d6e1c23e61964216f8
JSDID <icegouse@gmail.com>
2025-08-14 23:10:01 +0300

    Fix: import path

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
 import React from "react";
 import GetExample from "./components/then/rest/GetExample";
 import GetExampleAxios from "./components/then/rest/GetExampleAxios";
-import PostExample from "./components/rest/then/PostExample";
-import PostExampleAxios from "./components/then/rest/PatchExampleAxios";
-import PutExample from "./components/rest/then/PutExample";
+
+import PostExample from "./components/then/rest/PostExample";
+import PostExampleAxios from "./components/then/rest/PostExampleAxios";
+
+import PutExample from "./components/then/rest/PutExample";
 import PutExampleAxios from "./components/then/rest/PutExampleAxios";
+
 import PatchExample from "./components/then/rest/PatchExample";
 import PatchExampleAxios from "./components/then/rest/PatchExampleAxios";
+
 import DeleteExample from "./components/then/rest/DeleteExample";
 import DeleteExampleAxios from "./components/then/rest/DeleteExampleAxios";
 
-import GraphQLGetExample from "./components/graphql/then/GraphQLGetExample";
-import GraphQLPostExample from "./components/graphql/then/GraphQLPostExample";
+import GraphQLGetExample from "./components/then/graphql/GraphQLGetExample";
+import GraphQLPostExample from "./components/then/graphql/GraphQLPostExample";
 import GraphQLSubscriptionExample from "./components/then/graphql/GraphQLSubscriptionExample";
+
 
 function App() {
   return (
     <div style={{ padding: "20px" }}>
-      <h1>Примеры с then в React</h1>
-
       {/* REST API */}
       <section>
         <h2>REST API — Fetch</h2>
